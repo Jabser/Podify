83c4bc7f6b20e1c9ddc1bb511e182f9ba8193885
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-06-03 19:45:31 +0200

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './sass/app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './views/Home';
+import About from './views/About';
+
+import Footer from './components/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Router>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
