f4c4a11ae9b0955bd22f611f799c43ba6a7e3255
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-06-13 10:18:10 +0200

    Home - Initial page markup

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import '../sass/home.scss';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Home</h1>
+        <div className="container">
+          <h1 className="text-center">Search for podcasts</h1>
+          <form action="">
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="ex. genre" />
+              <div className="input-group-append">
+                <button className="btn btn-secondary" type="button">Search</button>
+              </div>
+            </div>
+          </form>
+          <div className="search-genres">
+            <a className="btn btn-link" href="">Business</a>
+            <i class="fas fa-circle"></i>
+            <a className="btn btn-link" href="">Comedy</a>
+            <i class="fas fa-circle"></i>
+            <a className="btn btn-link" href="">Crypto</a>
+            <i class="fas fa-circle"></i>
+            <a className="btn btn-link" href="">Politics</a>
+            <i class="fas fa-circle"></i>
+            <a className="btn btn-link" href="">History</a>
+            <i class="fas fa-circle"></i>
+          </div>
+        </div>
       </div>
     )
   }
