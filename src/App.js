e66e3c10c75f4d5f1243d61cfef387b6619c6e22
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-06-17 11:02:02 +0200

    Imported font awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/app.scss';
 
