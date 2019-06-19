6d53ced4162db1dea7463ce4175d7ee036167731
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-06-19 17:14:23 +0200

    Added support for active state on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Logo from '../images/podify-logo.png';
 
 class Navbar extends Component {
@@ -21,20 +21,20 @@ class Navbar extends Component {
         <nav class="navbar p-0 navbar-expand-md navbar-light">
           <div class="collapse navbar-collapse px-3" id="navbarContent">
             <ul class="navbar-nav mx-md-auto mx-0 w-50 align-items-start nav-fill">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">Home</Link>
+              <li className="nav-item">
+                <NavLink className="nav-link" exact={true} activeClassName="active" to="/">Home</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/">Popular</Link>
+                <NavLink className="nav-link" to="/popular">Popular</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/">Genres</Link>
+                <NavLink className="nav-link" to="/genres">Genres</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <NavLink className="nav-link" to="/about">About</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/card">card</Link>
+                <NavLink className="nav-link" to="/card">card</NavLink>
               </li>
             </ul>
           </div>
