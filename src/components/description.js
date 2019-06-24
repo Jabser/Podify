304e29ec336d821d6ba46003334e17f7f307844c
Cabrina <cabrinaengedal@outlook.com>
2019-06-24 10:03:47 +0200

    card - added describtion component

---
+++ src/components/description.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Description extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde sit ea delectus nemo, quaerat eum sint eos officia ad non tempora natus est, ipsum numquam fugiat quidem praesentium atque velit!
+        </p>
+      </div>
+    )
+  }
+}
