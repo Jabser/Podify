946b13447ca99dedee7d5ef04def4a19ca4fa8ad
Cabrina <cabrinaengedal@outlook.com>
2019-06-25 12:16:46 +0200

    login - added styling

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import '../sass/login.scss'
 
 class Login extends Component {
   render() {
     return (
       <div className="login">
         <div className="container">
-          <h1>Login</h1>
+          <div className="loginbox">
+            <h1>Login</h1>
+            <form action="/">
+              <label htmlFor="email">YOUR EMAIL:</label>
+              <input type="text" name="email" id="email" placeholder="ex. jakob@mail.com"/>
+              <label htmlFor="password">YOUR PASSWORD:</label>
+              <input type="password" name="password" id="password" placeholder="************"/>
+              <input type="submit" name="submit" id="submit" value="LOGIN"/>
+            </form>
+            <p>Don't have an account? <a href="/">sign up here</a></p>
+          </div>
         </div>
       </div>
     )
