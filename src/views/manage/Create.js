a81a2f7fedad022ec0180d58b297c8e11d1ca52c
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-06-25 15:45:06 +0200

    Create - Imported axios & setup component structure

--- src/views/manage/Create.js
+++ src/views/manage/Create.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../../sass/manage.scss'
+import axios from 'axios';
 
 class Create extends Component {
   render() {
     return (
       <div className="create">
         <div className="container">
-          
+          <form action="">
+            {/* Title */}
+
+
+            {/* Description */}
+
+
+            {/* Itunes */}
+
+
+            {/* Web */}
+
+
+            {/* Play */}
+
+
+            {/* Rating - Like */}
+
+
+            {/* Rating - Dislike */}
+
+
+            {/* Picture */}
+
+
+            {/* Submit to DB */}
+
+          </form>
         </div>
       </div>
     )
