cff9d7afbcc127861566710a1af48bc48475863e
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-08-26 12:41:32 +0200

    Home - Moved search code to dedicated component

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,22 @@ class Search extends Component {
   render () {
     return (
       <div className="search">
-        
+        <form action="">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search..." id="inputDefault" autoFocus />
+          </div>
+        </form>
+        <div className="search-genres">
+            <a className="btn btn-link" href="https://listennotes.com">Business</a>
+            <i className="fas fa-circle"></i>
+            <a className="btn btn-link" href="https://listennotes.com">Comedy</a>
+            <i className="fas fa-circle"></i>
+            <a className="btn btn-link" href="https://listennotes.com">Crypto</a>
+            <i className="fas fa-circle"></i>
+            <a className="btn btn-link" href="https://listennotes.com">Politics</a>
+            <i className="fas fa-circle"></i>
+            <a className="btn btn-link" href="https://listennotes.com">History</a>
+          </div>
       </div>
     )
   }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../sass/home.scss';
 import Logo from '../images/podify-logo.png';
+import Search from '../components/Search';
 
 class Home extends Component {
   render() {
@@ -9,24 +10,7 @@ class Home extends Component {
         <div className="container">
           <img src={Logo} alt="Logo duuh" className="img-fluid logo mx-auto d-block" />
           <h1 className="home-header">Search for Podcasts</h1>
-          <form action="">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search..." id="inputDefault" autoFocus />
-            </div>
-          </form>
-
-
-          <div className="search-genres">
-            <a className="btn btn-link" href="https://listennotes.com">Business</a>
-            <i className="fas fa-circle"></i>
-            <a className="btn btn-link" href="https://listennotes.com">Comedy</a>
-            <i className="fas fa-circle"></i>
-            <a className="btn btn-link" href="https://listennotes.com">Crypto</a>
-            <i className="fas fa-circle"></i>
-            <a className="btn btn-link" href="https://listennotes.com">Politics</a>
-            <i className="fas fa-circle"></i>
-            <a className="btn btn-link" href="https://listennotes.com">History</a>
-          </div>
+          <Search />
         </div>
       </div>
     )
