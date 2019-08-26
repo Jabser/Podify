ca079a4ecd1bd76b472e03174ccc806d8bfab1a9
Jakob (Jabs) <jakobnk123@hotmail.dk>
2019-08-26 12:36:19 +0200

    Home - Finalized the new search UI
    Also completed some code refactoring.

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,34 +8,15 @@ class Home extends Component {
       <div className="home">
         <div className="container">
           <img src={Logo} alt="Logo duuh" className="img-fluid logo mx-auto d-block" />
-          {/* <h1 className="text-center">Search for Podcasts</h1> */}
+          <h1 className="home-header">Search for Podcasts</h1>
           <form action="">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search Podcasts..." id="inputDefault" autoFocus />
+              <input type="text" class="form-control" placeholder="Search..." id="inputDefault" autoFocus />
             </div>
           </form>
 
 
-          <br/><br/><br/><br/>
-
-
-          <form action="">
-            <div className="input-group mb-3">
-              <input type="text" className="form-control" placeholder="Search..." />
-              {/* <div className="input-group-append">
-                <button className="btn btn-secondary" type="button">Search</button>
-              </div> */}
-              {/* <span class="input-group-append">
-                <button class="btn btn-outline-secondary border-left-0" type="button">
-                    <i class="fa fa-search"></i>
-                </button>
-              </span> */}
-              <span class="input-group-append">
-                <div class="input-group-text"><i class="fa fa-search"></i></div>
-              </span>
-            </div>
-          </form>
-          {/* <div className="search-genres">
+          <div className="search-genres">
             <a className="btn btn-link" href="https://listennotes.com">Business</a>
             <i className="fas fa-circle"></i>
             <a className="btn btn-link" href="https://listennotes.com">Comedy</a>
@@ -45,7 +26,7 @@ class Home extends Component {
             <a className="btn btn-link" href="https://listennotes.com">Politics</a>
             <i className="fas fa-circle"></i>
             <a className="btn btn-link" href="https://listennotes.com">History</a>
-          </div> */}
+          </div>
         </div>
       </div>
     )
