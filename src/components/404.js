ecd710803a6110a8cdeb73bce5f9f3972c6f613a
Jakob (Jabs) <jakobnk123@hotmail.dk>
2020-02-03 20:23:47 +0100

    404 - Updated cover image

--- src/components/404.js
+++ src/components/404.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../sass/404.scss';
+import Cover from '../images/404/cover.svg';
 
 function NoMatch({ location }) {
   return (
     <div className="no-match">
       <div className="container">
-        <i class="far fa-frown fa-10x"></i>
-        <h1 className="display-1">404</h1>
+        <img src={Cover} className="img-fluid" alt="404 cover" />
         <h1>That page doesn't exist: <code>{location.pathname}</code></h1>
         <NavLink to="/" className="btn btn-back btn-lg">Take me home</NavLink>
       </div>
