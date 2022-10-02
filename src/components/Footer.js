2383605486e9eecbbc199cb5787727256cf00818
Jabs <8946581+Jabser@users.noreply.github.com>
2022-10-02 21:26:54 +0200

    Footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
 
+import Logo from '../images/logo.png';
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <div className="container">
-          <div className="footer-social">
-            <a href="https://facebook.com">
-              <i className="fab fa-facebook-square fa-2x"></i>
-            </a>
-            <a href="https://twitter.com">
-              <i className="fab fa-twitter-square fa-2x"></i>
-            </a>
-            <a href="https://instagram.com">
-              <i className="fab fa-instagram fa-2x"></i>
-            </a>
+          <div className="footer-main">
+            <div className="row">
+              <div className="col-md-3">
+                <img src={Logo} alt="logo" />
+              </div>
+              <div className="col-md-3">
+                <h5>Company</h5>
+                <ul>
+                  <li><a href="">About</a></li>
+                  <li><a href="">Jobs</a></li>
+                  <li><a href="">News</a></li>
+                  <li><a href="">Pricing</a></li>
+                  <li><a href="">Contact</a></li>
+                </ul>
+              </div>
+              <div className="col-md-3">
+                <h5>Listen</h5>
+                <ul>
+                  <li><a href="">Episodes</a></li>
+                  <li><a href="">Trending</a></li>
+                  <li><a href="">Channels</a></li>
+                </ul>
+              </div>
+              <div className="col-md-3">
+                <h5>Useful links</h5>
+                <ul>
+                  <li><a href="">Support</a></li>
+                  <li><a href="">Login</a></li>
+                  <li><a href="">Sign up</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className="footer-legal">
-            &copy; 2019 - 2020 Podify - Powered by the <a href="https://www.listennotes.com/" rel="noopener noreferrer" target="_blank">Listen Notes API</a>
+            <div className="privacy-links">
+              <a href="" rel="noopener noreferrer">Legal</a>
+              <a href="" rel="noopener noreferrer">Privacy Policy</a>
+              <a href="" rel="noopener noreferrer">Cookies</a>
+            </div>
+            <div className="podify-legal">
+              &copy; 2019 - 2022 Podify - Powered by the <a href="https://www.listennotes.com/" rel="noopener noreferrer" target="_blank">Listen Notes API</a>
+            </div>
           </div>
         </div>
       </div>
