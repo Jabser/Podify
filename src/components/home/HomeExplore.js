238a6e73a49d7a383ce738a6d960c268fc99409d
Jabs <8946581+Jabser@users.noreply.github.com>
2022-11-02 18:17:54 +0100

    Home explorer
    Preliminary work

---
+++ src/components/home/HomeExplore.js
@@ -0,0 +1,14 @@
+import react, { Component } from "react";
+import '../../sass/home.scss';
+
+class HomeExplore extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default HomeExplore;
