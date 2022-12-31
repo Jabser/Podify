636365b93af484262537d6229bbc6b220864ecc2
Jabs <8946581+Jabser@users.noreply.github.com>
2022-12-31 13:22:59 +0100

    Added proper links to buttons
    115 +

--- src/views/Home.js
+++ src/views/Home.js
@@ -33,15 +33,15 @@ class Home extends Component {
                       The best podcast websites communicate a feel and make it easy for visitors to discover your podcast
                     </h4>
                     <div className="col-md-12 text-start home-cta">
-                      <button type="button" className="btn btn-primary me-3">
+                      <a href='/episodes' type="button" className="btn btn-primary me-3">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height={14} width={14} ><g><line x1={0.5} y1={1.5} x2={0.5} y2={12.5} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><line x1={5.7} y1={3.5} x2={5.7} y2={10.5} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><line x1={3.1} y1={5} x2={3.1} y2={9} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><line x1={13.5} y1={1.5} x2={13.5} y2={12.5} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><line x1={10.9} y1={3.5} x2={10.9} y2={10.5} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><line x1={8.3} y1={5} x2={8.3} y2={9} fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /></g></svg>
                         
                         Start Listening
-                      </button>
-                      <button type="button" className="btn btn-text">
+                      </a>
+                      <a href='/channels' type="button" className="btn btn-text">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" height={14} width={14} ><g><path d="M6.15.53a.39.39,0,0,0-.4,0,.26.26,0,0,0-.06.34C6.92,3,7.18,5.9,5.5,7.5A5.52,5.52,0,0,1,4,5.5,3.89,3.89,0,0,0,2,9a4.7,4.7,0,0,0,5,4.5c3.22,0,4.89-2,5-4.5C12.13,6,10,2.31,6.15.53Z" fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /><path d="M9.5,9a2,2,0,0,1-2,2" fill="none" stroke="#212529" strokeLinecap="round" strokeLinejoin="round" /></g></svg>
                         Explore channels
-                      </button>
+                      </a>
                     </div>
                   </div>
                   <div className="col-md-6 home-featured">
