2723b58800bb7d4f1c5c6f11a4df847493bded03
Jabs <8946581+Jabser@users.noreply.github.com>
2023-01-01 22:55:11 +0100

    Grid setup

--- src/views/Login.js
+++ src/views/Login.js
@@ -13,17 +13,18 @@ class Login extends Component {
             <h1>
               Login
             </h1>
-            <fieldset>
-              <div class="form-group">
-                <label for="exampleInputEmail1" class="col-form-label-lg mt-4">Email address</label>
-                <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1" class="col-form-label-lg mt-4">Password</label>
-                <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
-              </div>
-            </fieldset>
+            <h4>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad in laboriosam.
+            </h4>
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="col-form-label-lg mt-4">Email address</label>
+              <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1" class="col-form-label-lg mt-4">Password</label>
+              <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+            </div>
           </form>
         </div>
       </div>
